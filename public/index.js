398a1828e5de05723d22dbe61395128e282a8e8b
Cesar-i-Nada <cesar.alvarado.vega@gmail.com>
2025-02-27 04:53:23 -0600

    actualizacionM26

--- public/index.js
+++ public/index.js
@@ -4,11 +4,22 @@ function blases() {
 
 export{blases} 
 
+const snacks = ["🍎","🍕","🍫", "🍔", "🧁"]
+
+setInterval(()=>{
+  let listaSnacks = snacks.pop()
+  if(snacks.length > 0){
+  console.log(snacks)
+  }
+},1000)
+
+
+
 
 
 
 Swal.fire({
-    title: "Good job!",
-    text: "You clicked the button!",
+    title: "¡Estás dentro!",
+    text: "Tu registro ha sido exitoso",
     icon: "success"
   });
